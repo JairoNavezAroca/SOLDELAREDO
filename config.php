fdeedba9cc800e0601bb8177c2c8a1c56a060c5f
CintiaMO <58581626+CintiaMO@users.noreply.github.com>
2019-12-06 08:27:01 -0500

    cdecdf

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-	$Servidor = "localhost";
+	$Servidor = "localhost:50";
 	$User = "root";
 	$Password = "";
-	$BaseDeDatos = "pesifinal";
+	$BaseDeDatos = "sstareapesii";
 
 	//remotemysql.com/phpmyadmin/
-	$Servidor = "remotemysql.com";
-	$User = "kQSXCJnc3v";
-	$Password = "QEhvHHmuP9";
-	$BaseDeDatos = "kQSXCJnc3v";
+	//$Servidor = "remotemysql.com";
+	//$User = "kQSXCJnc3v";
+	//$Password = "QEhvHHmuP9";
+	//$BaseDeDatos = "kQSXCJnc3v";
 ?>
